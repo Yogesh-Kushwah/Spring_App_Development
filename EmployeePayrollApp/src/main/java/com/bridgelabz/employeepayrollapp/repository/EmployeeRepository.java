78ee8c540c9bc124696107d17c748d44f14b7650
Yogesh-Kushwah <159457528+Yogesh-Kushwah@users.noreply.github.com>
2025-02-27 12:03:26 +0530

    Delete EmployeePayrollApp directory
    Removing_redundant_directory

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz.employeepayrollapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.bridgelabz.employeepayrollapp.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
--- EmployeePayrollApp/src/main/java/com/example/SpringEmployeePayrollApp/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.SpringEmployeePayrollApp.repository;
-
-import com.example.SpringEmployeePayrollApp.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {}
