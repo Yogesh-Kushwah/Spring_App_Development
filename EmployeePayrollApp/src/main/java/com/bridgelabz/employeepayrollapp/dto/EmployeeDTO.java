726d31e851dbee063067a862cd55ac187230fff4
unknown <yogeshkushwah775@gmail.com>
2025-02-28 18:17:24 +0530

    [Yogesh Kushwah] Add. Database setting as Environment Variable

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
@@ -1,4 +1,4 @@
-package com.bridgelabz.employeePayrollApp.DTO;
+package com.bridgelabz.employeepayrollapp.dto;
 
 
 import jakarta.persistence.Entity;
--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/repository/EmployeeRepository.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/repository/EmployeeRepository.java
@@ -3,5 +3,6 @@ package com.bridgelabz.employeepayrollapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.bridgelabz.employeepayrollapp.model.Employee;
 
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
