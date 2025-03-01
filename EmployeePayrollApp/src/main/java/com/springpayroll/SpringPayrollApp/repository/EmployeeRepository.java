9fc66f58939bd731c8ee9317574f3f7590ac801e
unknown <yogeshkushwah775@gmail.com>
2025-03-01 08:27:55 +0530

    [Yogesh Kushwah] Add. Adding Remaining Properties to DTO and Model

--- EmployeePayrollApp/src/main/java/com/springpayroll/SpringPayrollApp/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springpayroll.SpringPayrollApp.repository;
-
-import com.springpayroll.SpringPayrollApp.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
