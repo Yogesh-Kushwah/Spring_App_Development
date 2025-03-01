6779943f4af52710c469ac4a5a93f9a1ade57ac3
unknown <yogeshkushwah775@gmail.com>
2025-03-01 10:38:24 +0530

    [Yogesh Kushwah] Add.Save Employee Payroll Data into MySql Database using JPARepository

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/repository/EmployeeRepository.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/repository/EmployeeRepository.java
@@ -1,8 +1,10 @@
 package com.bridgelabz.employeepayrollapp.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.bridgelabz.employeepayrollapp.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    // No additional methods needed for now
 }
